232a465aa3ae230be8c42a6ded1adf267c48ff00
StasGostev <spaceomen@gmail.com>
2019-06-12 17:32:43 +0300

    Updates

---
+++ precache-manifest.60152ace46b09cb5cff5219ee568f37e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48c82b78ebacd2decf873807a2ddf43",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "c04d7e1cf1da3c1645b1",
+    "url": "/react_dynamic-list-of-posts/static/css/main.685086f8.chunk.css"
+  },
+  {
+    "revision": "4e46e9d638db29490995",
+    "url": "/react_dynamic-list-of-posts/static/js/2.20e50229.chunk.js"
+  },
+  {
+    "revision": "c04d7e1cf1da3c1645b1",
+    "url": "/react_dynamic-list-of-posts/static/js/main.d2af23e3.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
